1491147b52218b2b507c209bf29e685606aa23b1
Samuel Prigent <samuel.prigent@yahoo.fr>
2025-05-04 20:07:38 +0200

    update loader timer

--- front/src/app/utils/loading.util.ts
+++ front/src/app/utils/loading.util.ts
@@ -1,6 +1,6 @@
 import { timer, Observable } from 'rxjs';
 
-export const DEFAULT_LOADING_DELAY = 300;
+export const DEFAULT_LOADING_DELAY = 260;
 
 export function loadingDelay$(ms: number = DEFAULT_LOADING_DELAY): Observable<number> {
   return timer(ms);
